1bffb67f540926048537eba9725bd493ca1950d1
Germán Escobar <german.escobarc@gmail.com>
2018-04-25 20:04:46 -0500

    Initial Import

---
+++ app.js
@@ -0,0 +1,31 @@
+var flights = JSON.parse('[{"airline":"Oceanic","status":"crashed","departure":{"iata":"SYD","time":"2004-09-22 14:55"}},{"airline":"Delta","status":"on_time","departure":{"iata":"JFK","time":"2018-04-26 12:34"},"arrival":{"iata":"SFO","time":"2018-04-26 18:55"}},{"airline":"American","status":"landed","departure":{"iata":"LAX","time":"2018-04-25 14:55"},"arrival":{"iata":"BOG","time":"2018-04-25 18:55"}}]');
+
+for (var i=0; i < flights.length; i++) {
+  var flight = flights[i];
+
+  var arrivalIata = flight.arrival ? flight.arrival.iata : "";
+  var arrivalTime = flight.arrival ? flight.arrival.time : "";
+
+  var className = "";
+  if (flight.status == "crashed") {
+    className = "table-danger";
+  } else if (flight.status == "landed") {
+    className = "table-success";
+  }
+
+  var context = {
+    airline: flight.airline,
+    status: flight.status,
+    departureIata: flight.departure.iata,
+    departureTime: flight.departure.time,
+    arrivalIata: arrivalIata,
+    arrivalTime: arrivalTime,
+    className: className
+  }
+
+  var html = $("#flight-template").html();
+  var template = Handlebars.compile(html);
+  var tr = template(context);
+
+  $("tbody").append(tr);
+}
